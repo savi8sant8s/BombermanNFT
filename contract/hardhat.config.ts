713860d299708606abccbbf703451830ab69ffaf
Sávio Santos <saviosa08@gmail.com>
2022-12-08 10:32:00 -0300

    Primeiro deploy do nft

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,6 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY as string;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -9,6 +13,12 @@ const config: HardhatUserConfig = {
       optimizer: {
         enabled: true,
       },
+    },
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
     },
   },
 };
